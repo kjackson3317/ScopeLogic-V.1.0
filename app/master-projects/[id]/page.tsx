e0ce26ce02b7b414a6c273336ee85105d1971fc7
kjackson3317 <m7ckhdrjt4@privaterelay.appleid.com>
2026-09-06 13:54:47 -0400

    RC5.7 use restored Master Project workspace

--- app/master-projects/[id]/page.tsx
+++ app/master-projects/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import { connection } from 'next/server';
 import { createClient, isSupabaseConfigured } from '../../../lib/supabase/server';
-import MasterWorkspaceClient from './master-workspace-client';
+import MasterWorkspaceClientV2 from './master-workspace-client-v2';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
@@ -21,7 +21,7 @@ export default async function MasterWorkspacePage({ params }: { params: Promise<
     .eq('id', user.id)
     .maybeSingle();
 
-  return <MasterWorkspaceClient
+  return <MasterWorkspaceClientV2
     masterProjectId={resolved.id}
     actualUserId={user.id}
     workspaceOwnerId={String(profile?.workspace_owner_id || user.id)}
