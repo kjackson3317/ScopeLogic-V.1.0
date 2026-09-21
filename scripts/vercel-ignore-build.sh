58fae500a82051f79c99a3fc154d6bb0ce564949
kjackson3317 <m7ckhdrjt4@privaterelay.appleid.com>
2026-09-21 16:40:44 -0400

    Allow isolated preview branch QA build

--- scripts/vercel-ignore-build.sh
+++ scripts/vercel-ignore-build.sh
@@ -5,7 +5,7 @@ PRODUCTION_PROJECT_ID="prj_TFZgqoOD7pnm1tC0LefH8QF6An5W"
 PREVIEW_PROJECT_ID="prj_fwTBCNUFRDKl3CUSFxp9wkkQiFlO"
 
 if [ "$VERCEL_PROJECT_ID" = "$PRODUCTION_PROJECT_ID" ]; then
-  if [ "$VERCEL_GIT_COMMIT_REF" = "main" ]; then
+  if [ "$VERCEL_GIT_COMMIT_REF" = "main" ] || [ "$VERCEL_GIT_COMMIT_REF" = "preview" ]; then
     exit 1
   fi
   exit 0
