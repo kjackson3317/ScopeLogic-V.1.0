6450b898580a893d00764a604b8e1c31fab8706d
kjackson3317 <m7ckhdrjt4@privaterelay.appleid.com>
2026-09-21 13:01:50 -0400

    Demo diagnostic: verify review release controller module wiring

---
+++ app/review-release-controller.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { REVIEW_RELEASE_OPTIONS } from './review-release-pdf';
+
+export default function ReviewReleaseController() {
+  void REVIEW_RELEASE_OPTIONS;
+  return null;
+}
