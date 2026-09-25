329b2246268c01b4fbaaa3c037c6ab80c3b56608
kjackson3317 <m7ckhdrjt4@privaterelay.appleid.com>
2026-09-25 01:16:46 -0400

    Remove obsolete PDF.js compatibility declaration

--- apps/takeoff-desktop/src/pdfjs-compat.d.ts
+++
@@ -1,8 +0,0 @@
-import 'pdfjs-dist';
-import type { PageViewport, RenderTask } from 'pdfjs-dist';
-
-declare module 'pdfjs-dist' {
-  interface PDFPageProxy {
-    render(params: { canvasContext: CanvasRenderingContext2D; viewport: PageViewport }): RenderTask;
-  }
-}
